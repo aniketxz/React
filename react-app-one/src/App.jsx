d07f9a75d0b1058bffde965adf0024d4ae49792f
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2025-03-01 21:05:18 +0530

    created movie app

--- react-app-one/src/App.jsx
+++ react-app-one/src/App.jsx
@@ -1,11 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState, useEffect} from 'react'
+
+const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
+
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  }, [hasLiked]);
+
+  /*
+  useEffect(() => {
+    console.log('CARD RENDERED')
+  }, []);
+  */
+  
+  return (
+    <div className="card" onClick={() => setCount(count + 1)} >
+      <h2>{title} <br/> {count || null}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "❤️" : "🤍"}
+      </button>
+    </div>
+  )
+} 
 
 const App = () => {
   return (
-    <h2>Functional Arrow Component</h2>
+    <div className="card-container">
+      <Card title="Star Wars" rating={5} isCool={true} />
+      <Card title="Avatar" />
+      <Card title="The Lion King" />
+    </div>
   )
 }
 
